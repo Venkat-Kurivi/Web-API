cb634001661870a738b135984fef483864438053
Venkateswarlu Kurivi <vkurivi@jnetind.com>
2018-07-30 15:54:09 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using SimpleWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleWebAPI.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public IHttpActionResult Get()//List<Person>
+        {
+            try
+            {
+                //Create Error for test api 
+                //int x = 0, y=1;
+                //int z = y/ x;
+
+                List<Person> list = new List<Person>();
+                list.Add(new Person() { Id = 1, FirstName = "Venkat", LastName = "Kurivi", Description = "" });
+                list.Add(new Person() { Id = 2, FirstName = "Hanish", LastName = "Maram", Description = "" });
+                list.Add(new Person() { Id = 3, FirstName = "Praveen", LastName = "Acha", Description = "" });
+                list.Add(new Person() { Id = 4, FirstName = "Kalyan", LastName = "Goru", Description = "" });
+                list.Add(new Person() { Id = 5, FirstName = "Azeem", LastName = "Abdul", Description = "" });
+                list.Add(new Person() { Id = 6, FirstName = "Manideep", LastName = "Manda", Description = "" });
+                return Ok(list);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace SimpleWebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // Return only JSON.
+            GlobalConfiguration.Configuration.Formatters.Clear();
+            GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
+        }
+    }
+}
